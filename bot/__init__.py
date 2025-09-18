7380a0d9071d2523ea715f194f045a80b4c4e2e6
kelenetwork <4223111@gmail.com>
2025-09-19 02:14:51 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,18 @@
+"""
+Telegram 机器人模块
+"""
+
+from .handlers import MessageHandler
+from .commands import CommandHandler
+from .keyboards import KeyboardBuilder
+from .callbacks import CallbackHandler
+
+__all__ = [
+    'MessageHandler',
+    'CommandHandler', 
+    'KeyboardBuilder',
+    'CallbackHandler'
+]
+
+__version__ = '1.0.0'
 
