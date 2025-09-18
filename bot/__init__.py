91d6168cf65a476d956d653e4acd8c1501c4d756
kelenetwork <4223111@gmail.com>
2025-09-19 02:26:46 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,18 +1,27 @@
 """
-Telegram 机器人模块
+Telegram Bot 模块
+包含机器人的核心功能和组件
 """
 
-from .handlers import MessageHandler
+from .bot_client import TelegramBot, BotManager
 from .commands import CommandHandler
-from .keyboards import KeyboardBuilder
-from .callbacks import CallbackHandler
+from .callbacks import CallbackHandler  
+from .handlers import MessageHandler, EventHandler, ErrorHandler
+from .keyboards import KeyboardBuilder, DynamicKeyboard, ContextualKeyboard
 
 __all__ = [
+    'TelegramBot',
+    'BotManager', 
+    'CommandHandler',
+    'CallbackHandler',
     'MessageHandler',
-    'CommandHandler', 
+    'EventHandler', 
+    'ErrorHandler',
     'KeyboardBuilder',
-    'CallbackHandler'
+    'DynamicKeyboard',
+    'ContextualKeyboard'
 ]
 
 __version__ = '1.0.0'
-
+__author__ = 'Auto Checkin Bot'
+__description__ = 'Telegram自动签到机器人'
